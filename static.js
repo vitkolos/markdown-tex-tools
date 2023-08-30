5946c209bb2781b0f8f8348a18b98a6cc6531511
Vít Kološ <vit.kolos@gmail.com>
2023-08-30 22:25:20 +0200

    Refactor static.js to be more general

--- static.js
+++ static.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 
 function getFile(urlParts, pathOffset, res) {
-    if (urlParts[pathOffset] == 'cards.js' || urlParts[pathOffset] == 'cards.css') {
+    const extension = urlParts[pathOffset].split('.').pop();
+
+    const supportedExtensions = {
+        js: 'application/javascript',
+        css: 'text/css',
+    };
+
+    if (extension in supportedExtensions) {
         fs.readFile('./static/' + urlParts.slice(pathOffset).join('/'), (err, data) => {
             if (err) {
                 fileNotFound(res);
             } else {
-                if (urlParts[pathOffset] == 'cards.js') {
-                    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/css' });
-                }
+                res.writeHead(200, { 'Content-Type': supportedExtensions[extension] });
                 res.end(data);
             }
         });
