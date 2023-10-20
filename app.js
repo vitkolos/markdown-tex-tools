b7af88e061c098f501a4ed7bc4bc9d5864364b9f
Vít Kološ <vit.kolos@gmail.com>
2023-10-20 10:46:05 +0200

    Add logging

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 // debugging: docker logs node
 
 const http = require('http');
+const fs = require('fs');
 const markview = require('./markview');
 const static = require('./static');
 
 const requestListener = function (req, res) {
+    const stopwatchStart = performance.now();
+    const logFile = fs.createWriteStream(__dirname + '/debug.log', { flags: 'a' });
+
     // where the real path starts; e.g. /node/ = 1, /script/node/app/ = 3
     const pathOffset = 1;
     const urlWithoutQuery = req.url.replace(/\?.*$/, '');
@@ -36,6 +40,10 @@ const requestListener = function (req, res) {
             res.end('page not found');
             break;
     }
+
+    const stopwatchEnd = performance.now();
+    const date = new Date();
+    logFile.write(date.toISOString() + '\t' + urlWithoutQuery + '\t' + (stopwatchEnd - stopwatchStart) + '\t' + req.headers['user-agent'] + '\n');
 }
 
 const server = http.createServer(requestListener);
